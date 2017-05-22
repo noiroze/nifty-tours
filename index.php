7f639c9cb9930febe9b8d06d9e9bdbacec4e429f
erica <noiroze@users.noreply.github.com>
2017-05-22 11:56:15 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Erica
+ * Date: 5/7/17
+ * Time: 11:51 AM
+ */
+include_once('includes/header.php');
+?>
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+
+
+                <div class="col-md-8">
+                    <section id="griddemo" class="section group">
+                        <div class="col span_1_of_2">
+                            <h1>Heading 1</h1>
+                            <p>paragraph text paragraph text paragraph text paragraph text paragraph text </p>
+                            <p>paragraph text paragraph text paragraph text paragraph text paragraph text </p>
+                            <ul>
+                                <li>List item (li)</li>
+                                <li>List item (li)</li>
+                                <li>List item (li)</li>
+                                <li>List item (li)</li>
+                                <li>List item (li)</li>
+                                <li>List item (li)</li>
+                            </ul>
+                            <h2>Heading 2</h2>
+                            <p>paragraph text paragraph text paragraph text paragraph text paragraph text </p>
+                            <p>paragraph text paragraph text paragraph text paragraph text paragraph text </p>
+                            <p>paragraph text paragraph text paragraph text paragraph text paragraph text </p>
+                        </div>
+                    </section>
+                </div>
+                <div class="col-md-4">
+
+                    <div id="social">
+                        <p>Social media feeds and Currency Converter will go in this column</p>
+                        <!--Currency Converter widget by FreeCurrencyRates.com -->
+                        <!--                        <div id="converter">-->
+                        <!--                        <div id='gcw_mainFvBWtfS1F' class='gcw_mainFvBWtfS1F'></div>-->
+                        <!--                        <a id='gcw_siteFvBWtfS1F' href='https://freecurrencyrates.com/en/'></a>-->
+                        <!--                        <script>function reloadFvBWtfS1F() {-->
+                        <!--                                var sc = document.getElementById('scFvBWtfS1F');-->
+                        <!--                                if (sc) sc.parentNode.removeChild(sc);-->
+                        <!--                                sc = document.createElement('script');-->
+                        <!--                                sc.type = 'text/javascript';-->
+                        <!--                                sc.charset = 'UTF-8';-->
+                        <!--                                sc.async = true;-->
+                        <!--                                sc.id = 'scFvBWtfS1F';-->
+                        <!--                                sc.src = 'https://freecurrencyrates.com/en/widget-vertical?iso=USDCNYMXNEURGBPXUL&df=2&p=FvBWtfS1F&v=fits&source=fcr&width=275&width_title=0&firstrowvalue=1&thm=C8C8C8,7F99B2,C8C8C8,330099,FFFFFF,C8C8C8,3333CC,FFFFFF,FFFFFF&title=Currency%20Converter&tzo=420';-->
+                        <!--                                var div = document.getElementById('gcw_mainFvBWtfS1F');-->
+                        <!--                                div.parentNode.insertBefore(sc, div);-->
+                        <!--                            }-->
+                        <!--                            reloadFvBWtfS1F(); </script>-->
+                        <!---->
+                        <!--                            </div>-->
+                        <!--                        <div id="fb">-->
+                        <!--                        <div id="fb-root">-->
+                        <!--                        <div class="fb-page" data-href="https://www.facebook.com/cas222cascade" data-tabs="timeline"-->
+                        <!--                             data-width="275" data-height="150" data-small-header="true"-->
+                        <!--                             data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">-->
+                        <!--                            <blockquote cite="https://www.facebook.com/cas222cascade" class="fb-xfbml-parse-ignore"><a-->
+                        <!--                                        href="https://www.facebook.com/cas222cascade">CAS 222</a></blockquote>-->
+                        <!--                        </div>-->
+                        <!--                        </div>-->
+                        <!--                        </div>-->
+                        <!--                        <div id="twitter">-->
+                        <!--                        <a class="twitter-timeline" href="https://twitter.com/pcccas222">Tweets by pcccas222</a>-->
+                        <!--                        </div>-->
+                        <!---->
+                        <!--                    </div>-->
+                        <!--                </div>-->
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </main>
+
+<?php include_once('includes/footer.php'); ?>
